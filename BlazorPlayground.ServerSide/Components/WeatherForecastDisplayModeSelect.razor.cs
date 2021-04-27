368a8a07634fd64bda606cbda871dee2448a3606
francisco.villena <francisco.villena@lpsystems.com>
2021-04-27 16:49:29 +0100

    Update Select to work with MudCheckBox

--- BlazorPlayground.ServerSide/Components/WeatherForecastDisplayModeSelect.razor.cs
+++ BlazorPlayground.ServerSide/Components/WeatherForecastDisplayModeSelect.razor.cs
@@ -5,38 +5,31 @@ using System.Threading.Tasks;
 namespace BlazorPlayground.ServerSide.Components
 {
     public partial class WeatherForecastDisplayModeSelect
-    {        
+    {
         [Parameter]
         public WeatherForecastDisplayModeSelection WeatherForecastDisplayModeSelection { get; set; }
 
         [Parameter]
         public EventCallback<WeatherForecastDisplayModeSelection> WeatherForecastDisplayModeSelectionChanged { get; set; }
 
-        protected void HandleInvalidSubmit()
+        //protected void HandleInvalidSubmit()
+        //{
+
+        //}
+
+        //protected void HandleValidSubmit()
+        //{
+
+        //}
+
+        //protected void OnParamatersSet()
+        //{
+
+        //}
+
+        private async Task ChangeDetailedSelection()
         {
-
-        }
-
-        protected void HandleValidSubmit()
-        {
-
-        }
-
-        protected void OnParamatersSet()
-        {
-
-        }
-
-        private async Task ChangeSelection()
-        {
-            if (WeatherForecastDisplayModeSelection.Detailed == false)
-            {
-                WeatherForecastDisplayModeSelection.Detailed = true;
-            }
-            else
-            {
-                WeatherForecastDisplayModeSelection.Detailed = false;
-            }
+            WeatherForecastDisplayModeSelection.Detailed = WeatherForecastDisplayModeSelection.Detailed == false;
 
             await WeatherForecastDisplayModeSelectionChanged.InvokeAsync(WeatherForecastDisplayModeSelection);
         }
